9aeeb69083a9cf956b415c97aaa2c41e3b9ecdb8
Vitor Valentim <vitorvalentin840@gmail.com>
2025-07-09 20:08:28 -0300

    Colocando as keys em 1 arquivo .env

--- src/services/pinFileToIPFS.ts
+++ src/services/pinFileToIPFS.ts
@@ -1,11 +1,7 @@
 import * as FileSystem from "expo-file-system";
+import { PINATA_API_KEY, PINATA_SECRET_API_KEY } from "@env";
 
 export async function uploadVideoToIPFS(localUri: string): Promise<string> {
-  // Substitua pelos seus dados do Pinata
-  const PINATA_API_KEY = "de7af9cee9a0663d0792";
-  const PINATA_SECRET_API_KEY =
-    "5f11341399aca85b539f5e121b7c12779f3ec31c5f70df9cbf1acabfc17a78f0";
-
   const fileInfo = await FileSystem.getInfoAsync(localUri);
   if (!fileInfo.exists) {
     throw new Error("Arquivo não encontrado.");
@@ -15,12 +11,11 @@ export async function uploadVideoToIPFS(localUri: string): Promise<string> {
   const fileType = "video/mp4";
 
   const formData = new FormData();
-  // @ts-ignore
   formData.append("file", {
     uri: localUri,
     name: fileName,
     type: fileType,
-  });
+  } as any);
 
   const response = await fetch(
     "https://api.pinata.cloud/pinning/pinFileToIPFS",
@@ -29,7 +24,6 @@ export async function uploadVideoToIPFS(localUri: string): Promise<string> {
       headers: {
         pinata_api_key: PINATA_API_KEY,
         pinata_secret_api_key: PINATA_SECRET_API_KEY,
-        // NÃO defina Content-Type, o fetch faz isso automaticamente
       },
       body: formData,
     }
@@ -43,6 +37,5 @@ export async function uploadVideoToIPFS(localUri: string): Promise<string> {
     );
   }
 
-  // O hash CID estará em json.IpfsHash
   return `https://gateway.pinata.cloud/ipfs/${json.IpfsHash}`;
 }
